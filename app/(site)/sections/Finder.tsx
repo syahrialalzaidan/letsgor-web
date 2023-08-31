500085c5d9ff832162609c0b9ab91c233f8bdb2d
Ahmad Rizki <amrizjr@gmail.com>
2023-08-31 18:49:34 +0700

    ngetengahin search yg di homepage

--- app/(site)/sections/Finder.tsx
+++ app/(site)/sections/Finder.tsx
@@ -2,7 +2,7 @@ import SearchBar from "../components/SearchBar";
 
 const Finder = () => {
   return (
-    <div className="w-full min-h-[70vh] bg-[url('/landing/Lapangan.jpg')] bg-no-repeat bg-cover bg-top py-10">
+    <div className="flex justify-center items-center w-full min-h-[70vh] bg-[url('/landing/Lapangan.jpg')] bg-no-repeat bg-cover bg-top py-10">
       <div className="container mx-auto px-7 text-white text-center flex flex-col justify-center items-center h-full">
         <h1 className="font-semibold text-3xl md:text-4xl">
           Temukan Partner dan Lapangan Pilihanmu
